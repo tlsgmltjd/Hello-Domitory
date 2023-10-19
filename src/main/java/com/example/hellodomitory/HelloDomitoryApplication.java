57e0eac9d3f881d2d9fc207bd1935d49e3d5456c
신희성 <vvwv3092@gmail.com>
2023-10-19 15:16:04 +0900

    init

---
+++ src/main/java/com/example/hellodomitory/HelloDomitoryApplication.java
@@ -0,0 +1,13 @@
+package com.example.hellodomitory;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloDomitoryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HelloDomitoryApplication.class, args);
+    }
+
+}
