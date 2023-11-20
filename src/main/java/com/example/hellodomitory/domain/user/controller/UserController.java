d3f41d59778ec03eba1bc2d4ba7dd28f6d17f21c
신희성 <vvwv3092@gmail.com>
2023-11-20 09:46:19 +0900

    Update header setting

--- src/main/java/com/example/hellodomitory/domain/user/controller/UserController.java
+++ src/main/java/com/example/hellodomitory/domain/user/controller/UserController.java
@@ -13,7 +13,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/auth")
+@RequestMapping(path = "/auth", consumes = "application/json")
 @RequiredArgsConstructor
 public class UserController {
 
