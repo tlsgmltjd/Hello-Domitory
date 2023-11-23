79c694fd929c4d37580ac15f3cb45e2bee674420
신희성 <vvwv3092@gmail.com>
2023-11-23 13:54:12 +0900

    Update accesslog token validation

--- src/main/java/com/example/hellodomitory/domain/accessLog/controller/AccessLogController.java
+++ src/main/java/com/example/hellodomitory/domain/accessLog/controller/AccessLogController.java
@@ -2,7 +2,11 @@ package com.example.hellodomitory.domain.accessLog.controller;
 
 import com.example.hellodomitory.domain.accessLog.dto.request.ResetRequest;
 import com.example.hellodomitory.domain.accessLog.service.AccessLogService;
+import com.example.hellodomitory.global.Jwt.JwtUtil;
+import com.example.hellodomitory.global.exception.CustomException;
+import com.example.hellodomitory.global.exception.ErrorCode;
 import com.example.hellodomitory.global.util.MsgResponseDto;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -16,9 +20,13 @@ import java.util.Date;
 @RequiredArgsConstructor
 public class AccessLogController {
     private final AccessLogService accessLogService;
+    private final JwtUtil jwtUtil;
 
     @GetMapping("/reset")
-    public ResponseEntity<MsgResponseDto> accessLogInit() {
+    public ResponseEntity<MsgResponseDto> accessLogInit(HttpServletRequest request) {
+        String token = jwtUtil.getJwtFromHeader(request);
+        if (token == null || !jwtUtil.validateToken(token)) throw new CustomException(ErrorCode.NOT_OK_TOKEN);
+
         accessLogService.initAccessLog();
         return ResponseEntity.ok(new MsgResponseDto("초기화 완료", HttpStatus.NO_CONTENT.value()));
     }
--- src/main/java/com/example/hellodomitory/domain/accessLog/service/AccessLogService.java
+++ src/main/java/com/example/hellodomitory/domain/accessLog/service/AccessLogService.java
@@ -4,6 +4,10 @@ import com.example.hellodomitory.domain.accessLog.entity.AccessLog;
 import com.example.hellodomitory.domain.accessLog.repository.AccessLogRepository;
 import com.example.hellodomitory.domain.user.entity.UserEntity;
 import com.example.hellodomitory.domain.user.repository.UserRepository;
+import com.example.hellodomitory.global.Jwt.JwtUtil;
+import com.example.hellodomitory.global.exception.CustomException;
+import com.example.hellodomitory.global.exception.ErrorCode;
+import jakarta.servlet.http.HttpServletRequest;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -16,10 +20,10 @@ import java.util.stream.Collectors;
 @RequiredArgsConstructor
 public class AccessLogService {
     private final AccessLogRepository accessLogRepository;
-    private final UserRepository userRepository;
 
     @Transactional
     public void initAccessLog() {
+
         Integer savedDate = accessLogRepository.findById(1L).get().getLogTime().getDayOfMonth();
         if (savedDate >= LocalDate.now().getDayOfMonth()) throw new RuntimeException();
 
